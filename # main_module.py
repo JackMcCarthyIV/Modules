3fd0d085d648983c257a2410f1be0cd734c5dd85
JackMcCarthyIV <Jack.McCarthy@Zapatacompting.com>
2023-07-02 08:23:17 -0500

    Initial commit

---
+++ # main_module.py
@@ -0,0 +1,8 @@
+# main_module.py
+import sub_module
+
+def main():
+    sub_module.sub_function()
+
+if __name__ == "__main__":
+    main()
---
+++ # sub_module.py
@@ -0,0 +1,4 @@
+# sub_module.py
+
+def sub_function():
+    print("This is a function from the sub_module.")
